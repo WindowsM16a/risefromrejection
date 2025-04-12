7dfa6d836aea58bbf7f5a777cd46aef1d11ad931
WindowsM16a <wuvershayne@gmail.com>
2025-04-12 14:20:17 +0000

    turned of writing to db

--- main.js
+++ main.js
@@ -67,10 +67,10 @@ const date = new Date();
 // getData();
 
 // uploading and updating the local personalities (10 for now). i'll stop doing local stuff once i learn auth so only i can write to the db.
-personalities.forEach((person) => {
-	const personRef = ref(db, "personalities/" + person.id);
-	set(personRef, person);
-});
+// personalities.forEach((person) => {
+// 	const personRef = ref(db, "personalities/" + person.id);
+// 	set(personRef, person);
+// });
 
 // JavaScript for the navbar functionality
 document.addEventListener("DOMContentLoaded", function () {
